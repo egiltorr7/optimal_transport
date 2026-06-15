8a32d200c401c70b754031d464efce648c0d02e2
Emmanuel Torres <egiltorr@cos-10-164-5-113.science.purdue.edu>
2026-06-15 10:52:27 -0400

    gpu fixes

--- clean_setup/discretize_first/1d/config/cfg_ladmm_gaussian_gpu.m
+++ clean_setup/discretize_first/1d/config/cfg_ladmm_gaussian_gpu.m
@@ -10,4 +10,5 @@ function cfg = cfg_ladmm_gaussian_gpu()
     cfg.disc       = @disc_staggered_1st_gpu;
     cfg.projection = @proj_fokker_planck_banded_gpu;
     cfg.use_gpu    = true;
+    cfg.gpu_device = 1;    % device index (1-based); override in script if needed
 end
